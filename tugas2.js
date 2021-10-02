c7689b21d355d4cf33f4e699ba24379b3dab676a
Muhammad Rizki Wahyudie <rizkiwahyudie@gmail.com>
2021-10-02 18:03:14 +0700

    Tugas Javascript
    Tugas 2 - Niomic

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+var number = 5;
+var string = "ini string";
+
+var boolean = false;
+if(number=5)
+boolean = true;
+else
+boolean = false;
+
+var undifined;
+var kosong = "sebenarnya ada yaitu nilai string";
+kosong = null;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(undifined);
+console.log(kosong);
